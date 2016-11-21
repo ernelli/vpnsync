2f8f1b101f043f879588257e959c30f4de43e36f
Mattias Ernelli <mattias@ernelli.com>
2016-11-21 13:31:36 +0000

    Changed logging to slack channel, to be used to announce VPN endpoint

--- startup.sh
+++ startup.sh
@@ -2,17 +2,25 @@
 
 cd /home/pi/vpnsync
 
-sudo -u pi echo $(date) Starting up, running as $(whoami) > ./startup.log
+
+LOGFILE=./slack.pipe
+
+./slack-logger.sh &
+
+sudo -u pi echo $(date) Starting up, running as $(whoami) | tee ./startup.log | slack
+
+exit 0
+
 
 sudo -u pi ./ssh_agent.sh
 . /home/pi/agent.rc
 #sync repo
-sudo -u pi git pull >> ./startup.log
+#sudo -u pi git pull >> ./startup.log
 sudo -u pi ./port-forward.sh >> ./startup.log
 echo "bring br0 up..." >> ./startup.log
 ifup br0 >> ./startup.log
 echo "Start openvpn..." >> ./startup.log
 ./openvpn.sh
-echo "Announce public IP..." >> ./startup.log
-./announce.sh
+#echo "Announce public IP..." >> ./startup.log
+#./announce.sh
 echo "Alldone VPN node ready..." >> ./startup.log
