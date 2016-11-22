7e696e2cb0e1db13b24cbcdd0f66565355fef48c
maer01 <mattias.ernelli@comhem.com>
2016-11-22 13:53:25 +0100

    Removed ssl dev dependency, auto confirm all installaion steps

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-apt-get install openvpn
-apt-get install miniupnpc
-apt-get install bridge-utils
-apt-get install git
-apt-get install libssl-dev
+apt-get -y install openvpn
+apt-get -y install miniupnpc
+apt-get -y install bridge-utils
+apt-get -y install git
+#apt-get install libssl-dev
 
 
 #git clone https://github.com/AGWA/git-crypt.git
